6243deec97e4577a9b39816fe47fa0535ff42516
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2022-02-07 21:45:45 +0530

    :tada: vue vite base solution
    initial vue project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Placeholder from "./components/Placeholder.vue";
+</script>
+
+<template>
+  <Placeholder msg="Hello Vue 3 + TypeScript + Vite">
+    <section>initial</section>
+  </Placeholder>
+</template>
+
+<style></style>
---
+++ src/components/Placeholder.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup(props, ctx) {},
+});
+</script>
+
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
